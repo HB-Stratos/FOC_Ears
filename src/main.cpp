ea0d336d41e8cb45b89ec09e1aaccf6d9e1232a4
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2025-06-18 21:58:40 +0200

    implement basic motion reactance

--- src/main.cpp
+++ src/main.cpp
@@ -42,62 +42,73 @@ void setup()
     earL.setHomePos(0.5);
     earR.setHomePos(-2.9);
 
-        earL.init(2.14, Direction(-1));
+    earL.init(2.14, Direction(-1));
     earR.init(1.98, Direction(-1));
 
     // earL.init();
     // earR.init();
 }
 
+unsigned long lastImuUpdate = 0;
 unsigned long lastPrintTime = 0;
 
 void loop()
 {
-    float target_angle = 0.1 * PI * _sin(float(millis()) / 500 * PI);
+    float targetAngle = 0.1 * PI * _sin(float(millis()) / 500 * PI);
+    float targetR = 0;
+    float targetL = 0;
     if (digitalRead(PIN_BUTTON) == LOW)
     {
-        target_angle -= 1.8;
-        // target_angle = 0;
+        // targetAngle -= 1.8;
+        // targetAngle = 0;
 
-        target_angle = imuHandler.getVelocity().x() * 10;
+        // targetAngle = imuHandler.getVelocity().x() * 10;
 
-        target_angle = utils::mapf(imuHandler.getAbsoluteRotation().y(), -90, 0, 0, 1);
+        // float rollAngle = utils::mapf(imuHandler.getAbsoluteRotation().z(), 0, -PI, -1, 1);
+        // float pitchAngle = utils::mapf(imuHandler.getAbsoluteRotation().y(), PI / 2, -PI / 2, 1, -1);
+
+        targetL =
+            +utils::remap_clamped(imuHandler.getAbsoluteRotation().z(), 0, -PI / 2, 0.9, 0.1) + utils::remap_clamped(imuHandler.getAbsoluteRotation().y(), PI / 2, 0, 0.9, 0.1);
+
+        targetR =
+            +utils::remap_clamped(imuHandler.getAbsoluteRotation().z(), -PI, -PI / 2, 0.9, 0.1) + utils::remap_clamped(imuHandler.getAbsoluteRotation().y(), PI / 2, 0, 0.9, 0.1);
     }
 
-    // earL.moveTo(target_angle);
-    // earR.moveTo(target_angle);
+    // earL.moveTo(targetAngle);
+    // earR.moveTo(targetAngle);
 
-    earL.move01(target_angle);
-    earR.move01(1 - target_angle);
+    earL.move01(targetL);
+    earR.move01(1 - targetR);
 
     earL.update();
     earR.update();
 
-    if (millis() % 10 == 0)
+    if (millis() - lastImuUpdate >= 10)
     {
         imuHandler.update();
+        lastImuUpdate = millis();
     }
 
     if (millis() - lastPrintTime >= 100)
     {
-        Serial.print("L: ");
-        Serial.print(earL.getAngle());
-        Serial.print(" R: ");
-        Serial.print(earR.getAngle());
+        // Serial.print("L: ");
+        // Serial.print(earL.getAngle());
+        // Serial.print(" R: ");
+        // Serial.print(earR.getAngle());
         Serial.print(" Target: ");
-        Serial.print(target_angle);
-        Serial.println("");
+        Serial.print(targetAngle);
+        // Serial.println("");
 
         lastPrintTime = millis();
 
-        // imu::Vector<3> var = imuHandler.getAbsoluteRotation();
-        // Serial.print("X: ");
-        // Serial.print(var.x());
-        // Serial.print(" Y: ");
-        // Serial.print(var.y());
-        // Serial.print(" Z: ");
-        // Serial.print(var.z());
-        // Serial.println("");
+        imu::Vector<3> var = imuHandler.getAbsoluteRotation();
+        Serial.print("X: ");
+        Serial.print(var.x());
+        Serial.print(" Y: ");
+        Serial.print(var.y());
+        Serial.print(" Z: ");
+        Serial.print(var.z());
+        Serial.println("");
     }
 
     /* Absolute Rotations:
